3f5bc817629a2c7d5995e1921c578c1262beb7a8
Chung Thế Hào <chungthehao@gmail.com>
2019-03-27 01:23:28 +0700

    Done

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,11 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'angular-practicing-directives';
+  showText = false;
+  logArr = [];
+  
+  toggleShowText() {
+  	this.showText = !this.showText;
+  	this.logArr.push(this.logArr.length + 1);
+  }
 }
